d122c7ab18ff858b21beb50fdb940f459fec2705
Eric Chung <ericlewischung@gmail.com>
2021-01-01 10:30:06 -0600

    work to steal features

---
+++ src/dto/classes/classes.dto.ts
@@ -0,0 +1,55 @@
+index: ""
+name: ""
+hit_die: int 
+proficiency_choices: [
+    {
+        choose: int 
+        type: ""
+        from: [
+            {
+                index: ""
+                name: ""
+                url: ""
+            }
+        ]
+    }
+]
+proficiencies: [
+    {
+        index: ""
+        name: ""
+        url: ""
+    }
+]
+saving_throws: [
+    {
+        index: ""
+        name: ""
+        url: ""
+    }
+]
+starting_eequipment: -url-
+class_levels: -url-
+subclasses: [
+    {
+        index: ""
+        name: ""
+        url: ""  <- might want for more info
+    }
+]
+spellcasting: {
+    info: [
+        {
+            desc: [ "" ]
+            name: ""
+        }
+    ]
+    level: int 
+    spellcasting_ability: {
+        index: ""
+        name: ""
+        url: "'"
+    }
+}
+spells: "url"
+---
