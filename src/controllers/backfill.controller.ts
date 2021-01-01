8e770a0857e4378bdf8a8e4e18c2b1119fca43cd
Eric Chung <eric.chung@bayer.com>
2021-01-01 13:14:50 -0600

    need to do so much

--- src/controllers/backfill.controller.ts
+++ src/controllers/backfill.controller.ts
@@ -10,42 +10,42 @@ export class BackfillController {
 
   @Get("/5eSpells")
   async backfillSpells(): Promise<void> {
-    return await this.backfillService.backfill("Spells", "/api/Spells");
+    return await this.backfillService.backfill("Spells", "/api/spells");
   }
 
   @Get("/5eFeatures")
   async backfillFeatures(): Promise<void> {
-    return await this.backfillService.backfill("Features", "/api/Features");
+    return await this.backfillService.backfill("Features", "/api/features");
   }
 
   @Get("/5eTraits")
   async backfillTraits(): Promise<void> {
-    return await this.backfillService.backfill("Features", "/api/Features");
+    return await this.backfillService.backfill("Traits", "/api/traits");
   }
 
   @Get("/5eClasses")
   async backfillClasses(): Promise<void> {
-    return await this.backfillService.backfill("Features", "/api/Features");
+    return await this.backfillService.backfill("Classes", "/api/classes");
   }
 
   @Get("/5eSubclasses")
   async backfillSubclasses(): Promise<void> {
-    return await this.backfillService.backfill("Features", "/api/Features");
+    return await this.backfillService.backfill("Subclasses", "/api/subclasses");
   }
 
   @Get("/5eRaces")
   async backfillRaces(): Promise<void> {
-    return await this.backfillService.backfill("Features", "/api/Features");
+    return await this.backfillService.backfill("Races", "/api/races");
   }
 
   @Get("/5eSubraces")
   async backfillSubraces(): Promise<void> {
-    return await this.backfillService.backfill("Features", "/api/Features");
+    return await this.backfillService.backfill("Subraces", "/api/subraces");
   }
 
   @Get("/5eItems")
   async backfillItems(): Promise<void> {
     //includes: /equipment /magic-items /weapon-properties
-    return await this.backfillService.backfill("Items", "/api/Features");
+    return await this.backfillService.backfill("Items", "/api/stuff");
   }
 }
