8db50ba0abbffc0c7051a222b9f5a77bdc8d870b
Eric Chung <eric.chung@bayer.com>
2021-01-08 10:01:40 -0600

    fixed paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import * as dotenv from "dotenv";
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { CharacterModule } from "./module/character.module";
@@ -5,6 +6,10 @@ import { AppService } from "./service/app.service";
 import { AppController } from "./controllers/app.controller";
 import { SpellsModule } from "./module/spells.module";
 import { BackfillModule } from "./module/backfill.module";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: __dirname + "/../.env" });
+}
 
 if (
   !process.env.DB_USER_NAME ||
@@ -25,7 +30,7 @@ if (
       username: process.env.DB_USER_NAME,
       password: process.env.DB_USER_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + "/db/entity/*{.ts,.js}"],
+      entities: [__dirname + "/**/**/*.entity.{js,ts}"],
       synchronize: true,
       logging: true,
       autoLoadEntities: true,
