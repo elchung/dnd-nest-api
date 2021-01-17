c9609bb5a70a784c0398633a60b22466b1fc771f
Eric C <ericlewischung@gmail.com>
2021-01-17 12:23:16 -0600

    checkpoint

---
+++ src/dto/Condition.dto.ts
@@ -0,0 +1,14 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { IsArray, IsString, IsOptional } from "class-validator";
+
+export class ConditionDto {
+  @IsArray()
+  @IsOptional()
+  @ApiPropertyOptional({ type: [String] })
+  description: string[];
+
+  @IsString()
+  @IsOptional()
+  @ApiPropertyOptional({ type: String })
+  name: string;
+}
